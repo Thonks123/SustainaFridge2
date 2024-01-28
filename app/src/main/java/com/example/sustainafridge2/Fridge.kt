3c6d685183f73effb1b4cb40506179c09e3a9755
Thonks123 <sande501@msu.edu>
2024-01-27 21:04:29 -0500

    Ported Classes

---
+++ app/src/main/java/com/example/sustainafridge2/Fridge.kt
@@ -0,0 +1,20 @@
+package com.example.sustainafridge2
+import com.example.sustainafridge2.Item
+
+class Fridge(invList: MutableList<Item>) {
+
+    private var inventoryList : MutableList<Item> = invList
+
+    private fun addItem(item: Item) {
+        //  inventoryList.add(item)
+    }
+
+    private fun addItemCam(){
+
+    }
+
+    private fun remove(){
+
+    }
+
+}
